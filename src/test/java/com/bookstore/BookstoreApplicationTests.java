dc578edd97b2a6eb0568e18ee4703f6846d84069
chenyuhjiuan <emilymarinovich@hotmail.com>
2019-10-22 16:08:51 -0400

    bookstore

--- src/test/java/com/bookstore/BookstoreApplicationTests.java
+++ src/test/java/com/bookstore/BookstoreApplicationTests.java
@@ -3,10 +3,14 @@ package com.bookstore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+/*@ContextConfiguration(classes = ElastSearchBootApplication.class)*/
+
 public class BookstoreApplicationTests {
 
 	@Test
